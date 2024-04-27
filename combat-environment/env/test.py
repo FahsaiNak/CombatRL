1d99cf34feb61cc387f699567f5a88148dc9b7cf
fahsain <fahsain.nak@gmail.com>
2024-04-26 23:06:02 -0600

    create env for single mutation that can be applied to 20 variants

--- combat-environment/env/test.py
+++ combat-environment/env/test.py
@@ -1,6 +1,7 @@
 from combat_environment import CombatEnvironment
+from combat_environment_v1 import CombatActionMaskedEnvironment
 from pettingzoo.test import parallel_api_test
 
 if __name__ == "__main__":
-    env = CombatEnvironment()
+    env = CombatActionMaskedEnvironment()
     parallel_api_test(env, num_cycles=1_000_000)
